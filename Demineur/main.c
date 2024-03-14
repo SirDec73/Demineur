5096b7da2839c95daeef979e387d434e306ab8aa
cbernard <cbernard@GCFRLY130>
2024-03-14 10:31:47 +0100

    Ajoutez des fichiers projet.

---
+++ Demineur/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+//Créer deux structures représentant la grille(Grid) et une case (Tile)
+
+
+typedef struct Grid
+{
+	int Width;
+	int Height;
+};
+
+
+typedef struct Tile {
+
+	int x;
+	int y;
+};
+
+//Créer une fonction permettant d’initialiser la grille et chaque case 
+
+
+
+
+
